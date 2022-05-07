341798deef36e68c529c43381d4879464e78e01a
deepinbubblegum <Bubbledeepin@outlook.co.th>
2022-05-07 22:31:09 +0700

    remove

--- libserver/include/server.hpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-class server
-{
-private:
-    std::string say_hello = "hello dll";
-public:
-    void sayhello(void);
-};
--- libserver/src/server.cpp
+++
@@ -1,5 +0,0 @@
-#include <server.hpp>
-
-void server::sayhello(void){
-    std::cout << say_hello << std::endl;
-}
