90978dcc74a2f5e967930e26230b5b22e6a1f9ce
deepinbubblegum <Bubbledeepin@outlook.co.th>
2022-05-09 03:33:02 +0700

    update

--- src/apps/apps.cpp
+++ src/apps/apps.cpp
@@ -6,4 +6,12 @@ int main(int, char**) {
     server serv_;
     serv_.say_hello();
     cout << "Hello, world!\n";
+    
+
+    // Press any key to continue...
+    #ifdef _WIN32
+        system("pause");
+    #elif __linux__
+        system("read");
+    #endif
 }
