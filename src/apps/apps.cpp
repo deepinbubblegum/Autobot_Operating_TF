de053d05748fb97257f7f55b9479f1c7da8c8fc7
deepinbubblegum <Bubbledeepin@outlook.co.th>
2022-05-16 23:10:23 +0700

    update

--- src/apps/apps.cpp
+++ src/apps/apps.cpp
@@ -6,14 +6,7 @@
 #include <controller.hpp>
 #include <autoloader.hpp>
 
-class Apps
-{
-private:
-
-public:
-
-};
-
+using namespace std;
 // int main(int, char**) {
 //     Server server;
 //     server.setDevice(0, "2000"); // device id and port server
@@ -33,3 +26,25 @@ public:
 //     server.stop_device();
 //     return 0;
 // }
+
+int main(int argc,char** argv) {
+    Autoloader autoload;
+    
+    YAML::Node UserConfig;
+
+
+    Server server;
+    int deviceID = 0;
+    string devicePort = "2000";
+    server.setDevice(deviceID, devicePort); // device id and port server
+    server.start_step();
+    vector<string> device_list = server.getDevice_list();
+    
+    string deviceName = device_list[deviceID];
+    cout << "device target name :" << deviceName << endl;
+    Controller ctl;
+    ctl.exec();
+
+    server.stop_device();
+    return 0;
+}
